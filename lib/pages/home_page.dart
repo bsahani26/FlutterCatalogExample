37401644610ced8a1f932f2b1cadf05439ed4dd4
Bikash Sahani <bsahani26@gmail.com>
2022-04-28 08:46:15 +0530

    Theme extracted

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,9 +7,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Flutter")
-      ),
+      appBar: AppBar(title: const Text("Flutter")),
       body: const Center(
         child: Text("Welcome to Flutter"),
       ),
