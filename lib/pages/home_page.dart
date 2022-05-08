a69f8df36b700d3e476a4312dbc8b9071607af83
Bikash Sahani <bsahani26@gmail.com>
2022-05-08 09:53:56 +0530

    Learned GridView and GridTile

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -39,10 +39,41 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: CatalogModel.items.isNotEmpty
-            ? ListView.builder(
+            ? GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: GridTile(
+                      header: Container(
+                        padding: const EdgeInsets.all(12),
+                        child: Text(
+                          item.name,
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        decoration:
+                            const BoxDecoration(color: Colors.deepPurple),
+                      ),
+                      child: Image.network(item.image),
+                      footer: Container(
+                        padding: const EdgeInsets.all(12),
+                        child: Text(
+                          item.price.toString(),
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        decoration:
+                            const BoxDecoration(color: Colors.deepOrange),
+                      ),
+                    ),
+                  );
+                },
                 itemCount: CatalogModel.items.length,
-                itemBuilder: (context, index) =>
-                    ItemWidget(item: CatalogModel.items[index]),
               )
             : const Center(
                 child: CircularProgressIndicator(),
